ed94d898a602782518354b278a450c7127751cdd
asishghos <ccasish01@gmail.com>
2024-10-25 22:21:32 +0530

    HD

--- lib/Infrastructure/api_routes/api_routes.dart
+++ lib/Infrastructure/api_routes/api_routes.dart
@@ -3,7 +3,8 @@ class ApiRoutes {
 
   static const String aboutusurl = '${baseurl}api/?format=json';
   static const String teamurl = '${baseurl}api/team/?format=json';
-  static const String eventurl = '${baseurl}api/events/2/?format=json';
+  static const String eventurl = '${baseurl}api/events/?format=json';
+  // https://recnitdgp.pythonanywhere.com/api/events/
   static const String signupurl = '${baseurl}api/users/register/?format=json';
   static const String signinurl = '${baseurl}/api/token/';
 }
